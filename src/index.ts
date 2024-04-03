56f137c5c0e2abaaa6978d2f49757f9378cbab4a
mkamiishi <mkamiishi@infragistics.com>
2024-04-03 15:25:58 +0900

    Initial commit
    初回コミット

---
+++ src/index.ts
@@ -0,0 +1,56 @@
+import { IgcLegendModule, IgcDataChartCoreModule, IgcDataChartCategoryModule, IgcDataChartCategoryCoreModule, IgcDataChartInteractivityModule, IgcDataChartAnnotationModule} from 'igniteui-webcomponents-charts';
+import { IgcLegendComponent, IgcDataChartComponent, IgcCategoryXAxisComponent, IgcNumericYAxisComponent, IgcLineSeriesComponent } from 'igniteui-webcomponents-charts';
+import { ContinentsBirthRateItem, ContinentsBirthRate } from './ContinentsBirthRate';
+
+import { ModuleManager } from 'igniteui-webcomponents-core';
+
+import "./index.css";
+
+ModuleManager.register(
+    IgcLegendModule,
+    IgcDataChartCoreModule,
+    IgcDataChartCategoryModule,
+    IgcDataChartCategoryCoreModule,
+    IgcDataChartInteractivityModule,
+    IgcDataChartAnnotationModule
+);
+
+export class Sample {
+
+    private legend: IgcLegendComponent
+    private chart: IgcDataChartComponent
+    private xAxis: IgcCategoryXAxisComponent
+    private yAxis: IgcNumericYAxisComponent
+    private lineSeries: IgcLineSeriesComponent
+    private _bind: () => void;
+
+    constructor() {
+        var legend = this.legend = document.getElementById('Legend') as IgcLegendComponent;
+        var chart = this.chart = document.getElementById('chart') as IgcDataChartComponent;
+        var xAxis = this.xAxis = document.getElementById('xAxis') as IgcCategoryXAxisComponent;
+        var yAxis = this.yAxis = document.getElementById('yAxis') as IgcNumericYAxisComponent;
+        var lineSeries = this.lineSeries = document.getElementById('lineSeries') as IgcLineSeriesComponent;
+
+        this._bind = () => {
+            chart.legend = this.legend;
+            xAxis.dataSource = this.continentsBirthRate;
+            lineSeries.dataSource = this.continentsBirthRate;
+            lineSeries.xAxis = this.xAxis;
+            lineSeries.yAxis = this.yAxis;
+        }
+        this._bind();
+
+    }
+
+    private _continentsBirthRate: ContinentsBirthRate = null;
+    public get continentsBirthRate(): ContinentsBirthRate {
+        if (this._continentsBirthRate == null)
+        {
+            this._continentsBirthRate = new ContinentsBirthRate();
+        }
+        return this._continentsBirthRate;
+    }
+
+}
+
+new Sample();
